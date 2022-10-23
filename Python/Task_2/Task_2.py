cb0940275b98af65e6ae8f91571bd1c9eef188ed
antoniovidov <91030382+antoniovidov@users.noreply.github.com>
2022-10-23 18:37:47 +0300

    Update Task_2.py

--- Python/Task_2/Task_2.py
+++ Python/Task_2/Task_2.py
@@ -1,3 +1,4 @@
+
 import io 
 import os
 import sys
@@ -10,12 +11,22 @@ import datetime
 
 def insert():   ##дефинираме функция
     answer = ""
+    thetop = "Name~~~~~~~~~~~Family~~~~~~~~~~~~~~Numb~Town~~~~~~~~~~~~~~~~GROMB_Date~~~~Scor"
+    value = bool
+    with open("bonbon.txt", "a") as f:
+        print("Welcome")
+    f.close()
+    with open("bonbon.txt","r") as f:
+        if thetop not in f.read():
+            value = True
+        else:
+            value = False
+    f.close()        
     with open("bonbon.txt", "a") as f: ## отваряме текстовия фаил за append
-
-
-        f.write( "Name~~~~~~~~~~~Family~~~~~~~~~~~~~~Numb~Town~~~~~~~~~~~~~~~~GROMB_Date~~~~Scor" +"\n")
-
-
+        if value == True:
+            f.write(thetop)
+        elif value == False:
+            f.write("\n")
         q = input("Do you want to make a new lineY/N-> ")
         if q=="N":
             print("bye bye!")
@@ -33,8 +44,8 @@ def insert():   ##дефинираме функция
             Name = Name+"~"
         f.seek(0)
         f.write(Name)
-
-
+        
+        
         Family = input("Enter your second name:")## вкарваме фамилно име под колона family и проверяваме дали е по-голямо от 20 символа
         while len(Family) > 20:
             print("Error: Family cannot be more then 20 characters,want to try again Y/N")
@@ -47,8 +58,8 @@ def insert():   ##дефинираме функция
             Family = Family+"~"
         f.seek(15)
         f.write(Family)
-
-
+        
+        
         Number = int(input("Enter your class number: "))## вкарваме число под колона Numb и проверяваме дали е по-голямо от 5 символа
         NumberS =""
         while len(NumberS) > 5:
@@ -63,8 +74,8 @@ def insert():   ##дефинираме функция
             NumberS = NumberS + "~"
         f.seek(35)
         f.write(NumberS)
-
-
+            
+            
         Town = input("Enter your Town name:")## вкарваме име на град под колона Town и проверяваме дали е по-голямо от 20 символа
         while len(Town) > 20:
             print("Error: Town name cannot be more then 20 characters,want to try again Y/N")
@@ -77,7 +88,7 @@ def insert():   ##дефинираме функция
             Town = Town+"~"
         f.seek(40)
         f.write(Town)
-
+        
         Gender = input("Enter your gender->M/W/N : ")## вкарваме пола под  колона Gender и проверяваме дали е по-голямо от 1 символ
         while len(Gender) > 1:
             print("Error: Gender  cannot be more then 1 character,want to try again Y/N")
@@ -88,8 +99,8 @@ def insert():   ##дефинираме функция
                 exit()
         f.seek(60)
         f.write(Gender)
-
-
+        
+        
         Room = int(input("Enter your room number "))## вкарваме пола под  колона Rom и проверяваме дали е по-голямо от 3 символa
         RoomS = ""
         RoomS = str(Room)
@@ -104,13 +115,13 @@ def insert():   ##дефинираме функция
             RoomS = RoomS + "~" 
         f.seek(61)
         f.write(RoomS)
-
+        
         d=input("When is your B'day? (in YYYY-MM-DD) ") ## вкарваме пола под  колона B_Date и проверяваме дали е по-голямо от 10 символa
         d=datetime.datetime.strptime(d,'%Y-%m-%d').date()##трябва да се направи try catch проверка
         dS = str(d)
         f.seek(64)
         f.write(dS)
-
+        
         score = float(input(" Number from 2 to 6 - > "))## вкарваме пола под  колона Score и проверяваме дали е по-голямо от 4 символa
         scoreS = ""
         scoreS = str(score)
@@ -125,10 +136,10 @@ def insert():   ##дефинираме функция
             scoreS = scoreS + "~"
         f.seek(74)
         f.write(scoreS)
-
-
-
-
+        
+        
+        
+        
     f.close()    
 
 insert()
